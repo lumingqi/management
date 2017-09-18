d814e6d7e9a8cc47e1c26531f56cd0494d209efb
zhangyingyi <1142112046@qq.com>
2017-09-18 17:05:04 +0800

    测试

--- backend/api/controllers/db/dboperations.js
+++ backend/api/controllers/db/dboperations.js
@@ -454,7 +454,8 @@ module.exports.download = function* download(db, name, next) {
             delete data[key]._id
             delete data[key].op_id
             delete data[key].op_name
-            var times = moment().format()
+                // var times = moment().format()
+            var times = getDatetime(data[key].time)
             console.log('时间123', times)
             var datat = []
             for (var keys in data[key]) {
