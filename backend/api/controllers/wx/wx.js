c43a8d5c626aa3082a0c647c094dba7981a92a58
隔壁老卢 <gebilaolu@gebilaoludeMacBook.local>
2017-09-20 10:34:57 +0800

    微信接口调试

--- backend/api/controllers/wx/wx.js
+++ backend/api/controllers/wx/wx.js
@@ -12,7 +12,7 @@ var dbstr = 'mongodb://localhost/'
 const querystring = require('querystring')
 var net = require('../../unit/net')
 var config = {
-    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+    debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
     appId: 'wx7e0aa09a76fe616b', // 必填，公众号的唯一标识
     timestamp: '', // 必填，生成签名的时间戳
     nonceStr: '', // 必填，生成签名的随机串
